918faee67f785fb5314e9e0b38dcb540d9b7bc1e
mr-n30 <69602501+mr-n30@users.noreply.github.com>
2020-12-16 15:59:46 -0600

    Added output to separate scan outputs

--- nmapperator.sh
+++ nmapperator.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Color for output
+GREEN="\e[32m";
+END="\e[0m";
+
 # Create directories to save nmap output
 SAVE_DIR=$2/nmap
 mkdir -p $SAVE_DIR
 
 # Scan all ports
-nmap -T4 -v -p- -oA $SAVE_DIR/all-ports $1
+echo -e "$GREEN###########################$END"
+echo -e "$GREEN### SCANNING ALL PORTS ####$END"
+echo -e "$GREEN###########################$END"
+nmap -T4 -v -p- -oA $SAVE_DIR/all-ports-scan $1
 
 # TODO:
 # Thinking about implemnting for more than one host
@@ -13,10 +20,16 @@ nmap -T4 -v -p- -oA $SAVE_DIR/all-ports $1
 # grep -oR 'Host:.*()' $SAVE_DIR/all-ports.gnmap | awk '/\s/ { print $2 }' | sort -u | tee -a $SAVE_DIR/hosts.txt
 
 # Grep for ports
-PORTS=$(grep -oR 'Ports:.*$' $SAVE_DIR/all-ports.gnmap | grep -oE '[0-9]{1,5}/' | sed 's/\///g' | sort -u | tr '\n' ',' | sed 's/,$//g')
+PORTS=$(grep -oR 'Ports:.*$' $SAVE_DIR/all-ports-scan.gnmap | grep -oE '[0-9]{1,5}/' | sed 's/\///g' | sort -u | tr '\n' ',' | sed 's/,$//g')
 
 # Do basic nmap scan on alive hosts and ports
+echo -e "$GREEN###################################$END"
+echo -e "$GREEN### DEFAULT SCAN ON OPEN PORTS ####$END"
+echo -e "$GREEN###################################$END"
 nmap -T4 -v -p $PORTS -sC -sV -oA $SAVE_DIR/default-scan $1
 
 # Do vuln scan on alive hosts and ports
+echo -e "$GREEN################################$END"
+echo -e "$GREEN### VULN SCAN ON OPEN PORTS ####$END"
+echo -e "$GREEN################################$END"
 nmap -T4 -v -p $PORTS --script vuln -sV -oA $SAVE_DIR/vuln-scan $1
