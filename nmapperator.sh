4f639e4f442dde2736ef9dffb98fc6ba03626b9b
mr-n30 <69602501+mr-n30@users.noreply.github.com>
2020-12-17 19:06:12 -0600

    Save directory is now user specified

--- nmapperator.sh
+++ nmapperator.sh
@@ -5,13 +5,13 @@ GREEN="\e[32m";
 END="\e[0m";
 
 # Create directories to save nmap output
-SAVE_DIR=$2/nmap
+SAVE_DIR=$2
 mkdir -p $SAVE_DIR
 
 # Scan all ports
-echo -e "$GREEN###########################$END"
-echo -e "$GREEN### SCANNING ALL PORTS ####$END"
-echo -e "$GREEN###########################$END"
+echo -e "$GREEN##########################$END"
+echo -e "$GREEN### SCANNING ALL PORTS ###$END"
+echo -e "$GREEN##########################$END"
 nmap -T4 -v -p- -oA $SAVE_DIR/all-ports-scan $1
 
 # TODO:
@@ -23,13 +23,13 @@ nmap -T4 -v -p- -oA $SAVE_DIR/all-ports-scan $1
 PORTS=$(grep -oR 'Ports:.*$' $SAVE_DIR/all-ports-scan.gnmap | grep -oE '[0-9]{1,5}/' | sed 's/\///g' | sort -u | tr '\n' ',' | sed 's/,$//g')
 
 # Do basic nmap scan on alive hosts and ports
-echo -e "$GREEN###################################$END"
-echo -e "$GREEN### DEFAULT SCAN ON OPEN PORTS ####$END"
-echo -e "$GREEN###################################$END"
+echo -e "$GREEN##################################$END"
+echo -e "$GREEN### DEFAULT SCAN ON OPEN PORTS ###$END"
+echo -e "$GREEN##################################$END"
 nmap -T4 -v -p $PORTS -sC -sV -oA $SAVE_DIR/default-scan $1
 
 # Do vuln scan on alive hosts and ports
-echo -e "$GREEN################################$END"
-echo -e "$GREEN### VULN SCAN ON OPEN PORTS ####$END"
-echo -e "$GREEN################################$END"
+echo -e "$GREEN###############################$END"
+echo -e "$GREEN### VULN SCAN ON OPEN PORTS ###$END"
+echo -e "$GREEN###############################$END"
 nmap -T4 -v -p $PORTS --script vuln -sV -oA $SAVE_DIR/vuln-scan $1
